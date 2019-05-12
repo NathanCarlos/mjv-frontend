22b647b366405b5454156254693aa4d5dd506442
nathancarlos12 <nathan.lima@fcamara.com.br>
2019-05-11 21:07:01 -0300

    Add navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule  } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { NavigationComponent } from './pages/website/navigation/navigation.component'
 
@@ -16,8 +16,8 @@ import { NavigationComponent } from './pages/website/navigation/navigation.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot(),
-    HttpClientModule 
+    HttpClientModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
